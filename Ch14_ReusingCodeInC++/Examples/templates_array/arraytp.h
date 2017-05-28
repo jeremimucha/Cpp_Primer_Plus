3be2dded170cc0f1adc4ba5b162a3be9d2088481
rockpaperbanan <rockpaperbanan@gmail.com>
2017-05-28 22:35:52 +0200

    Add Ch14 examples

---
+++ Ch14_ReusingCodeInC++/Examples/templates_array/arraytp.h
@@ -0,0 +1,50 @@
+// arraytp.h -- Array Template
+#ifndef ARRAYTP_H_
+#define ARRAYTP_H_
+
+#include <iostream>
+#include <cstdlib>
+
+
+template<typename T, int n>
+class ArrayTP
+{
+private:
+    T ar[n];
+public:
+    ArrayTP()
+        { std::cout << "--> ArrayTP()\n"; }
+    explicit ArrayTP(const T& v)
+        {
+            for(int i=0; i<n; ++i)
+                ar[i] = v;
+            std::cout << "--> ArrayTP(const T&)\n";
+        }
+    virtual T& operator[](int i);
+    virtual T  operator[](int i) const;
+};
+
+
+template<typename T, int n>
+T& ArrayTP<T,n>::operator[](int i)
+{
+    if(i<0 || i >= n){
+        std::cerr << "Error in array limits: " << i
+            << " is out of range\n";
+        std::exit(EXIT_FAILURE);
+    }
+    return ar[i];
+}
+
+template<typename T, int n>
+T ArrayTP<T,n>::operator[](int i) const
+{
+    if(i < 0 || i >= n){
+        std::cerr << "Error in array limits: " << i
+            << " is out of range\n";
+    }
+    return ar[i];
+}
+
+
+#endif /*ARRAYTP_H_*/
